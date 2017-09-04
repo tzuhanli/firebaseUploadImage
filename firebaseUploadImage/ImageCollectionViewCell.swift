19e717900fbec961065f3c4b815968b0c34eae03
Tzu Han Li <cathagice@gmail.com>
2017-09-04 14:56:57 +0800

    Feat: firebase sdk setup

---
+++ firebaseUploadImage/ImageCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ImageCollectionViewCell.swift
+//  firebaseUploadImage
+//
+//  Created by cora on 2017/9/1.
+//  Copyright © 2017年 cora. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
---
+++ firebaseUploadImage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  firebaseUploadImage
+//
+//  Created by cora on 2017/9/1.
+//  Copyright © 2017年 cora. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
